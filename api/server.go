b5d04efbd5eca084dcd59db22937660cde4289b9
ezequiel1988 <ekiolivier@gmail.com>
2020-02-18 10:08:19 -0300

    correccion en .env y server

--- api/server.go
+++ api/server.go
@@ -6,7 +6,7 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
-	"github.com/apicrud/controllers"
+	"github.com/apicrud/api/controllers"
 )
 
 var server = controllers.Server{}
@@ -22,8 +22,6 @@ func Run() {
 	}
 
 	server.Initialize(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
-
-	seed.Load(server.DB)
 
 	server.Run(":8080")
 
